5aa0123fe5fd170b3c0a3456cde2cdb7b473237f
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2023-02-20 19:11:46 +0530

    dzfdf

--- facebook/src/main/java/com/facebook/dao/FacebookDAO.java
+++ facebook/src/main/java/com/facebook/dao/FacebookDAO.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -48,24 +50,77 @@ public class FacebookDAO implements FacebooDAOInterface {
 	@Override
 	public int loginProfileDAO(FacebookUser fb) {
 		int i=0;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/vinoddatabase","root","rajesh");
-			
-			PreparedStatement ps=con.prepareStatement("select * from facebookuser where email=? and password=?");
-			ps.setString(1, fb.getEmail());
-			ps.setString(2, fb.getPassword());
-			
-			
-			ResultSet res=ps.executeQuery();
-			if(res.next()) {
-				i=1;
-			}
+		/*
+		 * try { Class.forName("com.mysql.jdbc.Driver"); Connection
+		 * con=DriverManager.getConnection("jdbc:mysql://localhost:3306/vinoddatabase",
+		 * "root","rajesh");
+		 * 
+		 * PreparedStatement ps=con.
+		 * prepareStatement("select * from facebookuser where email=? and password=?");
+		 * ps.setString(1, fb.getEmail()); ps.setString(2, fb.getPassword());
+		 * 
+		 * 
+		 * ResultSet res=ps.executeQuery(); if(res.next()) { i=1; } } catch(Exception e)
+		 * { e.printStackTrace(); }
+		 */
+		
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session s=sf.openSession();
+		
+		Query q=s.createQuery("from com.facebook.entity.FacebookUser f where f.email=:em and f.password=:pw");		
+		q.setParameter("em", fb.getEmail()); 
+		q.setParameter("pw", fb.getPassword());
+		List<FacebookUser> f=q.getResultList();
+		if(f.size()>0) {
+			i=1;
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+		
+		return i;
+	}
+	
+	public int deleteProfileDAO(FacebookUser fb) {
+		int i=0;
+		
+		
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session s=sf.openSession();
+		
+		Query q=s.createQuery("delete from com.facebook.entity.FacebookUser f where f.email=:em ");		
+		q.setParameter("em", fb.getEmail()); 
+		
+		i=q.executeUpdate();
+		
+		return i;
+	}
+	public int updateProfileDAO(FacebookUser fb) {
+		int i=0;
+		
+		
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session s=sf.openSession();
+		
+		Query q=s.createQuery("update com.facebook.entity.FacebookUser f  set f.password=:pw where f.email=:em ");		
+		q.setParameter("em", fb.getEmail()); 
+		q.setParameter("pw", fb.getPassword());
+		
+		i=q.executeUpdate();
+		
 		return i;
 	}
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
